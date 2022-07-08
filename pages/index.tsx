6f627154e85b940592ee496a5d9ca1b6ed951e05
ajimath <kotiryuichi2@outlook.jp>
2022-07-08 21:43:10 +0900

    comit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import Head from "next/head";
+import { useState } from "react";
 
 import type { NextPage } from "next";
 
+function useNabeatsu(): [number, () => void, string] {
+  const [count, setCount] = useState(0);
+
+ const countUp = () => setCount((prevCount) => prevCount + 1);
+ const initialState = count === 0;
+ const countMultipleOfThree = count % 3 === 0;
+ const countHasThree = Boolean(count.toString().match(/3/));
+ const isAho = !initialState && (countMultipleOfThree || countHasThree);
+ const nabeatsu = isAho ? "アホ" : "アホじゃない";
+    return [count, countUp, nabeatsu];
+   }
+
 const Home: NextPage = () => {
+   const [count, countUp, nabeatsu] = useNabeatsu();
+ 
   return (
     <div>
       <Head>
@@ -11,7 +26,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="m-0 min-h-screen">ローカルホストが立ち上がりました</main>
+      <main className="flex justify-center m-0">
+      <p>{nabeatsu}</p>       
+      <h1>{count}</h1>
+       <button onClick={countUp}>Count Up!</button>
+       </main>
     </div>
   );
 };
